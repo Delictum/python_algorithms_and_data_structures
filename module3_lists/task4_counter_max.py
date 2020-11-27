72b75ba0443649d5f38721df43732341f6e9dc0f
Delictum <thedrakoneragoni@gmail.com>
2020-11-27 20:31:38 +0300

    Added task for module6 - task completed successfully

--- module3_lists/task4_counter_max.py
+++ module3_lists/task4_counter_max.py
@@ -6,3 +6,10 @@ from random import randint
 
 mas = [randint(0, 9) for i in range(10)]
 print(mas, '\nМаксимальное: ', max(mas, key=mas.count))
+
+a = [randint(0, 9) for i in range(10)]
+max_index = 0
+for i in a:
+    if a.count(max_index) < a.count(i):
+        max_index = a.index(i)
+print(max_index)
